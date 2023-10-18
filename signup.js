8c9f9c60f177b1f03585c9a81aafed6970343c81
Ayush Dhruw <dhruwayush@gmail.com>
2023-10-18 09:02:13 +0530

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,32 +1,29 @@
-// Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAm4aDJ-g-UehALCEPvuzqNeLmQfUW4M8Q",
-  authDomain: "revisiontracker-dc145.firebaseapp.com",
-  projectId: "revisiontracker-dc145",
-  storageBucket: "revisiontracker-dc145.appspot.com",
-  messagingSenderId: "140685845306",
-  appId: "1:140685845306:web:3861b3a78048d253c46603",
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-// Initialize Firebase Authentication
-const auth = firebase.auth();
-
-// Function to sign up a user
 function signUpUser() {
   const emailInput = document.getElementById("email");
   const passwordInput = document.getElementById("password");
+  const nameInput = document.getElementById("name"); // Add this line
 
   const email = emailInput.value;
   const password = passwordInput.value;
+  const name = nameInput.value; // Add this line
 
   auth.createUserWithEmailAndPassword(email, password)
-    .then((user) => {
+    .then((userCredential) => {
       // Sign-up successful
-      // Redirect to the index page after signing up
-      window.location.href = "index.html";
+      const user = userCredential.user;
+      
+      // Update the user's profile with their name
+      user.updateProfile({
+        displayName: name,
+      }).then(() => {
+        // Profile updated successfully
+        window.location.href = "index.html";
+      }).catch((profileError) => {
+        // Handle profile update errors
+        const errorText = document.getElementById("errorText");
+        errorText.textContent = profileError.message;
+        errorText.classList.remove("hidden");
+      });
     })
     .catch((error) => {
       // Handle sign-up errors (e.g., display error message)
