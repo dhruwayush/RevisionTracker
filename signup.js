77bcedadb0d5dd5accc76a1b415d1ce9e1ca443f
Ayush Dhruw <dhruwayush@gmail.com>
2023-10-18 09:47:34 +0530

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,29 +1,15 @@
 function signUpUser() {
   const emailInput = document.getElementById("email");
   const passwordInput = document.getElementById("password");
-  const nameInput = document.getElementById("name"); // Add this line
 
   const email = emailInput.value;
   const password = passwordInput.value;
-  const name = nameInput.value; // Add this line
 
   auth.createUserWithEmailAndPassword(email, password)
     .then((userCredential) => {
       // Sign-up successful
-      const user = userCredential.user;
-      
-      // Update the user's profile with their name
-      user.updateProfile({
-        displayName: name,
-      }).then(() => {
-        // Profile updated successfully
-        window.location.href = "index.html";
-      }).catch((profileError) => {
-        // Handle profile update errors
-        const errorText = document.getElementById("errorText");
-        errorText.textContent = profileError.message;
-        errorText.classList.remove("hidden");
-      });
+      // Redirect to the index page after signing up
+      window.location.href = "index.html";
     })
     .catch((error) => {
       // Handle sign-up errors (e.g., display error message)
